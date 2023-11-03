3a7ed826faa68a69c58bce31a4322a78e362a8f0
Meryem Kalkan <127223707+meryem-kalkan@users.noreply.github.com>
2023-11-03 15:08:50 +0400

    add weekday and time to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,12 +3,17 @@ import './Header.css';
 
 export const Header = () => {
     const units = 0;
+    const time = new Date().toLocaleTimeString('default', {timeStyle: 'short',  hour12: false });
+    const weekday = new Date().toLocaleString(
+        'default', {weekday: 'long'}
+      );
+
     return (
         <div className="header-container">
         <div className="header">
         <h2>Skyly</h2>
         <input type="search"/>
-        <p>Monday 14:34</p>
+        <p>{weekday}  {time}</p>
         {units ? 'f' : '°C'}
         </div>
         <Outlet/>
