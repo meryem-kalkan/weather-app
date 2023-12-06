09976f9584373edfcf0e9cc5279c878412a36c7d
Meryem Kalkan <127223707+meryem-kalkan@users.noreply.github.com>
2023-12-06 22:16:51 +0400

    update store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import weatherReducer from '../features/Weather/WeatherSlice';
+import geoCodeReducer from '../features/GeoCode/GeoCodeSlice';
 
 export const store = configureStore({
   reducer: {
-    
+    weather: weatherReducer,
+    geoCode: geoCodeReducer
   },
 });
+
